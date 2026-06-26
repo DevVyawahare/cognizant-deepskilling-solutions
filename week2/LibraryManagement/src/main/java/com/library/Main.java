6b2be09f9b8d0d422014ef12f8b4d814a92aa8ea
Dev Vyawahare <vydev2005@gmail.com>
2026-06-26 14:30:17 +0530

    Complete Week 2: Exercise 4 Configured Maven Project with Spring Context, AOP, and WebMVC dependencies

--- week2/LibraryManagement/src/main/java/com/library/Main.java
+++ week2/LibraryManagement/src/main/java/com/library/Main.java
@@ -1,18 +1,19 @@
 package com.library;
 
-import com.library.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.library.service.BookService;
+
 public class Main {
     public static void main(String[] args) {
-        // 1. Initialize Spring Container (Fixed the typo here!)
+        // Initialize Spring Container
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // 2. Retrieve the managed singleton instance bean
+        // Retrieve the managed singleton instance bean
         BookService bookService = (BookService) context.getBean("bookService");
 
-        // 3. Trigger execution flow
+        // Trigger execution flow
         bookService.runBusinessLogic();
     }
 }
