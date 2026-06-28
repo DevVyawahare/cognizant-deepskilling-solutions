03ee7a7e784a156a7d635a0007ac8d072ba24a8f
Dev Vyawahare <vydev2005@gmail.com>
2026-06-29 00:46:47 +0530

    Complete Week 3: Implemented PathVariable based Country query web service with Lambda filtering

--- week3/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ week3/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -2,29 +2,32 @@ package com.cognizant.springlearn.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.Service.CountryService;
 
 @RestController
 public class CountryController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
 
-    @RequestMapping(value = "/country", method = RequestMethod.GET)
-    public Country getCountryIndia() {
-        LOGGER.info("--- START: getCountryIndia() invoked ---");
+    @Autowired
+    private CountryService countryService;
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+    // Existing /country handler can remain here...
 
-        Country country = context.getBean("country", Country.class);
-
-        LOGGER.info("--- END: getCountryIndia() execution completed ---");
-
+    // Dynamic path variable mapping targeting /country/{code}
+    @GetMapping("/country/{code}")
+    public Country getCountry(@PathVariable String code) {
+        LOGGER.info("--- START: getCountry() invoked for code: {} ---", code);
+        
+        Country country = countryService.getCountry(code);
+        
+        LOGGER.info("--- END: getCountry() completed ---");
         return country;
     }
 }
