12a43eee681023adf6bd5c381f59dffe0447b05d
Dev Vyawahare <vydev2005@gmail.com>
2026-06-29 00:16:48 +0530

    Complete Week 3: Hands-on 4 Spring Core XML Bean Configuration completed

--- week3/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ week3/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
@@ -12,6 +14,20 @@ public class SpringLearnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringLearnApplication.class, args);
-        LOGGER.info("Inside main method - Spring Boot 3 Web Server started successfully!");
+        LOGGER.info("Inside main method - Spring Web server initialized.");
+        
+        displayCountry();
+    }
+
+    public static void displayCountry() {
+        LOGGER.info("--- START: Loading Country Bean via XML Container ---");
+        
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        
+        Country country = context.getBean("country", Country.class);
+        
+        LOGGER.debug("Country Details: {}", country.toString());
+        
+        LOGGER.info("--- END: Country Bean Loaded Successfully ---");
     }
 }
